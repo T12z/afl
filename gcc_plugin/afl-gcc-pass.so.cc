12c79c06b9aca70ae9f2673e55de9ed315349dbc
Thorsten <stehbrettsegler@tractionpad.de>
2018-05-16 13:08:13 +0200

    sync, plugin crashes gcc

--- gcc_plugin/afl-gcc-pass.so.cc
+++ gcc_plugin/afl-gcc-pass.so.cc
@@ -5,6 +5,8 @@
    Written by Austin Seipp <aseipp@pobox.com> with bits from
               Emese Revfy <re.emese@gmail.com>
 
+   Updated by Thorsten Schulz <thorsten.schulz@uni-rostock.de>
+
    GCC integration design is based on the LLVM design, which comes
    from Laszlo Szekeres. Some of the boilerplate code below for
    afl_pass to adapt to different GCC versions was taken from Emese
@@ -46,13 +48,16 @@
 #include <gcc-plugin.h>
 #include <plugin-version.h>
 #include <diagnostic.h>
-#include <gimple.h>
 #include <tree.h>
 #include <tree-ssa.h>
 #include <tree-pass.h>
+#include <gimple.h>
+#include <gimple-iterator.h>
 #include <version.h>
 #include <toplev.h>
 #include <intl.h>
+#include <context.h>
+#include <stringpool.h>
 
 /* Eclipse Hack */
 #ifndef VERSION
@@ -66,157 +71,171 @@ static int be_quiet = 0;
 static unsigned int inst_ratio = 100;
 static int inst_blocks = 0;
 
-static unsigned int ext_call_instrument(void)
-{
-  /* Instrument all the things! */
-  basic_block bb;
+static unsigned int ext_call_instrument(function *fun) {
+	/* Instrument all the things! */
+	basic_block bb;
+	unsigned finst_blocks = 0;
+	unsigned fcnt_blocks = 0;
 
-  FOR_EACH_BB(bb) {
-    gimple g;
-    gimple_seq seq = NULL;
-    gimple_stmt_iterator bentry;
+	FOR_ALL_BB_FN(bb, fun) {
+		gimple *fcall = NULL;
+		gimple_seq seq = NULL;
+		gimple_stmt_iterator bentry;
 
-    /* Bail on this block if we trip the specified ratio */
+		fcnt_blocks++;
 
-    if (R(100) >= inst_ratio) continue;
+		/* Bail on this block if we trip the specified ratio */
+		if (R(100) >= inst_ratio) continue;
 
-    /* Make up cur_loc */
+		/* Make up cur_loc */
 
-    unsigned int rand_loc = R(MAP_SIZE);
-    tree cur_loc = build_int_cst(uint64_type_node, rand_loc);
+		unsigned int rand_loc = R(MAP_SIZE);
+		tree cur_loc = build_int_cst(uint64_type_node, rand_loc);
 
-    /* Update bitmap via external call */
+		/* Update bitmap via external call */
+		/* to quote:
+		 * /+ Trace a basic block with some ID +/
+		 * void __afl_trace(u16 x);
+		 */
 
-    tree fntype = build_function_type_list(
-      void_type_node,   /* return */
-      uint16_type_node, /* args */
-      NULL_TREE);
-    tree fndecl = build_fn_decl("__afl_trace", fntype);
-    TREE_STATIC(fndecl)     = 1; /* Defined elsewhere */
-    TREE_PUBLIC(fndecl)     = 1; /* Public */
-    DECL_EXTERNAL(fndecl)   = 1; /* External linkage */
-    DECL_ARTIFICIAL(fndecl) = 1; /* Injected by compiler */
+		tree fntype = build_function_type_list(
+			void_type_node,   /* return */
+			uint16_type_node, /* args */
+			NULL_TREE);       /* done */
+		tree fndecl = build_fn_decl("__afl_trace", fntype);
+		TREE_STATIC(fndecl)     = 1; /* Defined elsewhere */
+		TREE_PUBLIC(fndecl)     = 1; /* Public */
+		DECL_EXTERNAL(fndecl)   = 1; /* External linkage */
+		DECL_ARTIFICIAL(fndecl) = 1; /* Injected by compiler */
 
-    g = gimple_build_call(fndecl, 1, cur_loc);
-    gimple_seq_add_stmt(&seq, g);
+		fcall = gimple_build_call(fndecl, 1, cur_loc);  /* generate the function _call_ to above built reference, with *1* parameter -> the random const for the location */
+		//gimple_seq_add_stmt(&seq, fcall); /* and insert into a sequence */
 
-    /* Done - grab the entry to the block and insert sequence */
+		/* Done - grab the entry to the block and insert sequence */
+		bentry = gsi_start_bb(bb);
+		gsi_insert_seq_before(&bentry, seq, GSI_SAME_STMT);
 
-    bentry = gsi_start_bb(bb);
-    gsi_insert_seq_before(&bentry, seq, GSI_SAME_STMT);
+		inst_blocks++;
+		finst_blocks++;
+	}
 
-    inst_blocks++;
-  }
+	/* Say something nice. */
+	if (!be_quiet) {
+		if (!finst_blocks)
+			WARNF(G_("No instrumentation targets found in " cBRI "%s" cRST ),
+					function_name(fun));
+		else if (finst_blocks < fcnt_blocks)
+			OKF(G_("Instrumented %2u /%2u locations in " cBRI "%s" cRST ),
+					finst_blocks, fcnt_blocks,
+					function_name(fun));
+		else
+			OKF(G_("Instrumented   %2u   locations in " cBRI "%s" cRST ),
+					finst_blocks,
+					function_name(fun));
+	}
 
-  /* Say something nice. */
-  if (!be_quiet) {
-    if (!inst_blocks) WARNF(G_("No instrumentation targets found."));
-    else OKF(G_("Instrumented %u locations (%s mode, ratio %u%%)."),
-             inst_blocks,
-             getenv("AFL_HARDEN") ? G_("hardened") : G_("non-hardened"),
-             inst_ratio);
-  }
-
-  return 0;
+	return 0;
 }
 
-static unsigned int inline_instrument(void)
-{
-  /* Instrument all the things! */
-  basic_block bb;
+static unsigned int inline_instrument(function *fun) {
+	/* Instrument all the things! */
+	basic_block bb;
 
-  /* Set up global type declarations */
-  tree map_type = build_pointer_type(unsigned_char_type_node);
-  tree map_ptr_g = build_decl(BUILTINS_LOCATION, VAR_DECL,
-    get_identifier("__afl_area_ptr"), map_type);
-  TREE_USED(map_ptr_g)       = 1;
-  TREE_STATIC(map_ptr_g)     = 1; /* Defined elsewhere */
-  DECL_EXTERNAL(map_ptr_g)   = 1; /* External linkage */
-  DECL_PRESERVE_P(map_ptr_g) = 1;
-  DECL_ARTIFICIAL(map_ptr_g) = 1;
-  rest_of_decl_compilation(map_ptr_g, 1, 0);
+	/* Set up global type declarations */
+	tree map_type = build_pointer_type(unsigned_char_type_node);
+	tree map_ptr_g = build_decl(BUILTINS_LOCATION, VAR_DECL,
+			get_identifier("__afl_area_ptr"), map_type);
+	TREE_USED(map_ptr_g) = 1;
+	TREE_STATIC(map_ptr_g) = 1; /* Defined elsewhere */
+	DECL_EXTERNAL(map_ptr_g) = 1; /* External linkage */
+	DECL_PRESERVE_P(map_ptr_g) = 1;
+	DECL_ARTIFICIAL(map_ptr_g) = 1;
+	rest_of_decl_compilation(map_ptr_g, 1, 0);
 
-  tree prev_loc_g = build_decl(BUILTINS_LOCATION, VAR_DECL,
-    get_identifier("__afl_prev_loc"), uint16_type_node);
-  TREE_USED(prev_loc_g)       = 1;
-  TREE_STATIC(prev_loc_g)     = 1; /* Defined elsewhere */
-  DECL_EXTERNAL(prev_loc_g)   = 1; /* External linkage */
-  DECL_PRESERVE_P(prev_loc_g) = 1;
-  DECL_ARTIFICIAL(prev_loc_g) = 1;
-  rest_of_decl_compilation(prev_loc_g, 1, 0);
+	tree prev_loc_g = build_decl(BUILTINS_LOCATION, VAR_DECL,
+			get_identifier("__afl_prev_loc"), uint16_type_node);
+	TREE_USED(prev_loc_g) = 1;
+	TREE_STATIC(prev_loc_g) = 1; /* Defined elsewhere */
+	DECL_EXTERNAL(prev_loc_g) = 1; /* External linkage */
+	DECL_PRESERVE_P(prev_loc_g) = 1;
+	DECL_ARTIFICIAL(prev_loc_g) = 1;
+	rest_of_decl_compilation(prev_loc_g, 1, 0);
 
-  FOR_EACH_BB(bb) {
-    gimple g;
-    gimple_seq seq = NULL;
-    gimple_stmt_iterator bentry;
+	FOR_ALL_BB_FN(bb, fun) {
+		gimple *g;
+		gimple_seq seq = NULL;
+		gimple_stmt_iterator bentry;
 
-    /* Bail on this block if we trip the specified ratio */
+		/* Bail on this block if we trip the specified ratio */
 
-    if (R(100) >= inst_ratio) continue;
+		if (R(100) >= inst_ratio)
+			continue;
 
-    /* Make up cur_loc */
+		/* Make up cur_loc */
 
-    unsigned int rand_loc = R(MAP_SIZE);
-    tree cur_loc = build_int_cst(uint64_type_node, rand_loc);
+		unsigned int rand_loc = R(MAP_SIZE);
+		tree cur_loc = build_int_cst(uint64_type_node, rand_loc);
 
-    /* Load prev_loc, xor with cur_loc */
+		/* Load prev_loc, xor with cur_loc */
 
-    tree area_off = create_tmp_var(uint64_type_node, "area_off");
-    g = gimple_build_assign_with_ops(
-          BIT_XOR_EXPR, area_off, prev_loc_g, cur_loc);
-    gimple_seq_add_stmt(&seq, g); // area_off = prev_loc ^ cur_loc
+		tree area_off = create_tmp_var(uint64_type_node, "area_off");
+		g = gimple_build_assign(area_off, BIT_XOR_EXPR, prev_loc_g, cur_loc);
+		gimple_seq_add_stmt(&seq, g); // area_off = prev_loc ^ cur_loc
 
-    /* Update bitmap */
+		/* Update bitmap */
 
-    tree zero   = build_int_cst(unsigned_char_type_node, 0);
-    tree one    = build_int_cst(unsigned_char_type_node, 1);
+		tree zero = build_int_cst(unsigned_char_type_node, 0);
+		tree one = build_int_cst(unsigned_char_type_node, 1);
 
-    tree tmp1 = create_tmp_var(map_type, "tmp1");
-    g = gimple_build_assign_with_ops(PLUS_EXPR, tmp1, map_ptr_g, area_off);
-    gimple_seq_add_stmt(&seq, g); // tmp1 = __afl_area_ptr + area_off
+		tree tmp1 = create_tmp_var(map_type, "tmp1");
+		g = gimple_build_assign(tmp1, PLUS_EXPR, map_ptr_g, area_off);
+		gimple_seq_add_stmt(&seq, g); // tmp1 = __afl_area_ptr + area_off
 
-    tree tmp2 = create_tmp_var(unsigned_char_type_node, "tmp2");
-    tree deref1 = build_fold_indirect_ref(tmp1);
-    g = gimple_build_assign_stat(tmp2, deref1);
-    gimple_seq_add_stmt(&seq, g); // tmp2 = *tmp1
+		tree tmp2 = create_tmp_var(unsigned_char_type_node, "tmp2");
+		tree deref1 = build_fold_indirect_ref(tmp1);
+		g = gimple_build_assign(tmp2, INIT_EXPR, deref1);
+		gimple_seq_add_stmt(&seq, g); // tmp2 = *tmp1
 
-    tree tmp3 = create_tmp_var(unsigned_char_type_node, "tmp3");
-    g = gimple_build_assign_with_ops(PLUS_EXPR, tmp3, tmp2, one);
-    gimple_seq_add_stmt(&seq, g); // tmp3 = tmp2 + 1
+		tree tmp3 = create_tmp_var(unsigned_char_type_node, "tmp3");
+		g = gimple_build_assign(tmp3, PLUS_EXPR, tmp2, one);
+		gimple_seq_add_stmt(&seq, g); // tmp3 = tmp2 + 1
 
-    tree tmp4 = create_tmp_var(map_type, "tmp4");
-    g = gimple_build_assign_with_ops(PLUS_EXPR, tmp4, map_ptr_g, area_off);
-    gimple_seq_add_stmt(&seq, g); // tmp4 = __afl_area_ptr + area_off
+		tree tmp4 = create_tmp_var(map_type, "tmp4");
+		g = gimple_build_assign(tmp4, PLUS_EXPR, map_ptr_g, area_off);
+		gimple_seq_add_stmt(&seq, g); // tmp4 = __afl_area_ptr + area_off
 
-    tree deref2 = build2(MEM_REF, map_type, tmp4, zero);
-    g = gimple_build_assign_stat(deref2, tmp3);
-    //gimple_seq_add_stmt(&seq, g); // *tmp4 = tmp3
+		tree deref2 = build2(MEM_REF, map_type, tmp4, zero);
+		g = gimple_build_assign(deref2, MODIFY_EXPR, tmp3);
+		//gimple_seq_add_stmt(&seq, g); // *tmp4 = tmp3
 
-    /* Set prev_loc to cur_loc >> 1 */
+		/* Set prev_loc to cur_loc >> 1 */
 
-    tree shifted_loc = build_int_cst(TREE_TYPE(prev_loc_g), rand_loc >> 1);
-    g = gimple_build_assign_stat(prev_loc_g, shifted_loc);
-    gimple_seq_add_stmt(&seq, g); // __afl_pred_loc = cur_loc >> 1
+		tree shifted_loc = build_int_cst(TREE_TYPE(prev_loc_g), rand_loc >> 1);
+		g = gimple_build_assign(prev_loc_g, MODIFY_EXPR, shifted_loc);
+		gimple_seq_add_stmt(&seq, g); // __afl_pred_loc = cur_loc >> 1
 
-    /* Done - grab the entry to the block and insert sequence */
+		/* Done - grab the entry to the block and insert sequence */
 
-    bentry = gsi_start_bb(bb);
-    gsi_insert_seq_before(&bentry, seq, GSI_SAME_STMT);
+		bentry = gsi_start_bb(bb);
+		gsi_insert_seq_before(&bentry, seq, GSI_SAME_STMT);
 
-    inst_blocks++;
-  }
+		inst_blocks++;
+	}
 
-  /* Say something nice. */
-  if (!be_quiet) {
-    if (!inst_blocks) WARNF(G_("No instrumentation targets found."));
-    else OKF(G_("Instrumented %u locations (%s mode, ratio %u%%)."),
-             inst_blocks,
-             getenv("AFL_HARDEN") ? G_("hardened") : G_("non-hardened"),
-             inst_ratio);
-  }
+	/* Say something nice. */
+	if (!be_quiet) {
+		if (!inst_blocks)
+			WARNF(G_("No instrumentation targets found."));
+		else
+			OKF(G_("Instrumented %u locations (%s mode, ratio %u%%)."),
+					inst_blocks,
+					getenv("AFL_HARDEN") ? G_("hardened") : G_("non-hardened"),
+					inst_ratio);
+	}
 
-  return 0;
+	return 0;
 }
+
 
 /* -------------------------------------------------------------------------- */
 /* -- Boilerplate and initialization ---------------------------------------- */
@@ -238,24 +257,27 @@ static const struct pass_data afl_pass_data = {
                 // include new PHI nodes for newly added symbols or
                 // names. Do not remove this. Do not taunt Happy Fun
                 // Ball.
-                .todo_flags_finish      = TODO_update_ssa | TODO_verify_ssa | TODO_cleanup_cfg,
+                .todo_flags_finish      = TODO_update_ssa | TODO_verify_il | TODO_cleanup_cfg,
 };
 
 namespace {
 
 class afl_pass : public gimple_opt_pass {
-public:
-	afl_pass() : gimple_opt_pass(afl_pass_data, g) {}
+private:
+	bool do_ext_call;
 
-	virtual unsigned int execute(function *) {
-		return instrument();
+public:
+	afl_pass(bool ext_call, gcc::context *g) : gimple_opt_pass(afl_pass_data, g), do_ext_call(ext_call) {}
+
+	virtual unsigned int execute(function *fun) {
+		return do_ext_call ? ext_call_instrument(fun) : inline_instrument(fun);
 	}
 }; /* class afl_pass */
 
 }  /* anon namespace */
 
-static struct opt_pass *make_afl_pass(void) {
-	return new afl_pass();
+static struct opt_pass *make_afl_pass(bool ext_call, gcc::context *ctxt) {
+	return new afl_pass(ext_call, ctxt);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -269,45 +291,51 @@ static struct plugin_info afl_plugin_info = {
 };
 
 int plugin_init(struct plugin_name_args *plugin_info,
-                struct plugin_gcc_version *version)
-{
-  const char * const pname = plugin_info->base_name;
-  struct register_pass_info afl_pass_info;
-  struct timeval tv;
-  struct timezone tz;
-  u32 rand_seed;
+                struct plugin_gcc_version *version) {
 
-  /* Setup random() so we get Actually Random(TM) outputs from R() */
-  gettimeofday(&tv, &tz);
-  rand_seed = tv.tv_sec ^ tv.tv_usec ^ getpid();
-  srandom(rand_seed);
+	const char * const pname = plugin_info->base_name;
+	struct register_pass_info afl_pass_info;
+	struct timeval tv;
+	struct timezone tz;
+	u32 rand_seed;
 
-  /* Pass information */
-  afl_pass_info.pass                            = make_afl_pass();
-  afl_pass_info.reference_pass_name             = "ssa";
-  afl_pass_info.ref_pass_instance_number        = 1;
-  afl_pass_info.pos_op                          = PASS_POS_INSERT_AFTER;
+	/* Setup random() so we get Actually Random(TM) outputs from R() */
+	gettimeofday(&tv, &tz);
+	rand_seed = tv.tv_sec ^ tv.tv_usec ^ getpid();
+	srandom(rand_seed);
 
-  if (!plugin_default_version_check(version, &gcc_version)) {
-    FATAL(G_("Incompatible gcc/plugin versions!"));
-  }
+	/* Pass information */
+	afl_pass_info.pass = make_afl_pass(true, g);
+	afl_pass_info.reference_pass_name = "ssa";
+	afl_pass_info.ref_pass_instance_number = 1;
+	afl_pass_info.pos_op = PASS_POS_INSERT_AFTER;
 
-  /* Show a banner */
-  if (isatty(2) && !getenv("AFL_QUIET")) {
-    SAYF(G_(cCYA "afl-gcc-pass " cBRI VERSION cRST " by <aseipp@pobox.com>\n"));
-  } else be_quiet = 1;
+	if (!plugin_default_version_check(version, &gcc_version)) {
+		FATAL(G_("Incompatible gcc/plugin versions!"));
+	}
 
-  /* Decide instrumentation ratio */
-  char* inst_ratio_str = getenv("AFL_INST_RATIO");
+	/* Show a banner */
+	if (isatty(2) && !getenv("AFL_QUIET")) {
+		SAYF(G_(cCYA "afl-gcc-pass " cBRI VERSION cRST " by <aseipp@pobox.com>, updated by <thorsten.schulz@uni-rostock.de>\n"));
+	} else
+		be_quiet = 1;
 
-  if (inst_ratio_str) {
-    if (sscanf(inst_ratio_str, "%u", &inst_ratio) != 1 || !inst_ratio ||
-        inst_ratio > 100)
-      FATAL(G_("Bad value of AFL_INST_RATIO (must be between 1 and 100)"));
-  }
+	/* Decide instrumentation ratio */
+	char* inst_ratio_str = getenv("AFL_INST_RATIO");
 
-  /* Go go gadget */
-  register_callback(pname, PLUGIN_INFO, NULL, &afl_plugin_info);
-  register_callback(pname, PLUGIN_PASS_MANAGER_SETUP, NULL, &afl_pass_info);
-  return 0;
+	if (inst_ratio_str) {
+		if (sscanf(inst_ratio_str, "%u", &inst_ratio) != 1 || !inst_ratio || inst_ratio > 100)
+			FATAL(G_("Bad value of AFL_INST_RATIO (must be between 1 and 100)"));
+		else {
+			if (!be_quiet)
+				ACTF(G_("Instrumention ratio %u%% in %s mode."),
+					inst_ratio,
+					getenv("AFL_HARDEN") ? G_("hardened") : G_("non-hardened"));
+		}
+	}
+
+	/* Go go gadget */
+	register_callback(pname, PLUGIN_INFO, NULL, &afl_plugin_info);
+	register_callback(pname, PLUGIN_PASS_MANAGER_SETUP, NULL, &afl_pass_info);
+	return 0;
 }
